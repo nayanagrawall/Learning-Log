98a6cb014c8ab70494b715d170e9aa82694239ff
Nayan Agrawal <nayan55506@gmail.com>
2023-11-18 17:48:59 +0530

    Used Django-Bootstrap5 to give some style to the webpages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -29,15 +29,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'learning_logs',
+    'accounts',
+
+    # Third party apps.
+    'django_bootstrap5',
+
+    # Default django apps.
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # My apps
-    'learning_logs',
-    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Calcutta"
 
 USE_I18N = True
 
