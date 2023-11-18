cfed2866a66f962c537c2df7a2c24334bfe304e3
Nayan Agrawal <nayan55506@gmail.com>
2023-11-18 15:48:54 +0530

    Made topics user dependent

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about"""
-    
+
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User,
+                              on_delete=models.CASCADE)  # Added an owner field to Topic, which establishes a foreign key relationship to the User Model. If user is deleted, all the topics associated with that user will be deleted as well.
 
     def __str__(self):
         """Return a string representation of the model"""
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -13,7 +14,8 @@ def index(request):
 @login_required  # This will prevent unauthenticated user to request a page protected by it. Only login user can access.
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by(
+        'date_added')  # filtering the topics based on the user who has added that topic
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -22,6 +24,10 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belogs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by(
         '-date_added')  # - sign in front of date_added sorts the results in reverse order
     context = {'topic': topic, 'entries': entries}
@@ -38,7 +44,9 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)  # commit False because we have to modify the new topic before saving
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')  # This code will bring back the topic page after adding new topic
     # Display a blank or invalid form.
     context = {'form': form}
@@ -49,6 +57,9 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Adding a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # No data submitted, create a blank form.
@@ -72,6 +83,10 @@ def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    # Make sure the entry belongs to the current user
+    if topic.owner != request.user:
+        raise Http404  # This will display an error message.
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
